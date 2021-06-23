2e90ff7031e94b3f0bbc7b6bde6de0af121f50db
Ivan Cubilla <ivanctsp@hotmail.com>
2021-06-22 21:57:26 -0300

    update

--- practica/Concatenacion/src/Concatenacion.java
+++
@@ -1,9 +0,0 @@
-public class Concatenacion {
-
-    public static void main(String[] args) {
-
-        String nombre = "Juan";
-
-        System.out.println("Hola " + nombre + " bienvenido");
-    }
-}
--- practica/Constante/src/Constante.java
+++
@@ -1,9 +0,0 @@
-public class Constante {
-
-    public static void main(String[] args) {
-
-        final double VALOR_PI = 3.1416;
-
-        System.out.println("VALOR_PI = " + VALOR_PI);
-    }
-}
